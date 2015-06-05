ecd5dbf258ddc783df275f1ae953a861d0c6070c
Nikolay Suknyarov <suknyarov@gmail.com>
2015-06-05 16:31:27 +0300

    Added new model - hd 125 evo.

--- skuter-hd-125.php
+++ skuter-hd-125.php
@@ -2,10 +2,10 @@
 <h2>HD 125 EVO</h2>
 <div class="container">
 <div class="row-fluid">
-	<div class="span3 pagination-centered">
+	<div class="span5 pagination-centered">
 	<img class="img-polaroid" src="resources/images/hd-125/sym-hd-125-1.jpg" alt="скутер SYM HD 125 EVO"/>
 	</div>
-	<div class="span9">
+	<div class="span7">
 		<h4 class="text-blue">Скутер от висока класа</h4>
 			<p>
 			</p>
@@ -18,13 +18,43 @@
 		<div id="links">
 		<ul class="thumbnails pagination-centered">
 			<a href="resources/images/hd-125/sym-hd-125-1.jpg" title="SYM HD 125 EVO">
-			<img src="resources/images/hd-125/sym-hd-125-1.jpg" class="img-polaroid" alt="SYM HD 125 EVO image1" style="width: 100px; height: 100px;">
+			<img src="resources/images/hd-125/sym-hd-125-1.jpg" class="img-polaroid" alt="SYM HD 125 EVO image1" style="width: 120px; height: 100px;">
 			</a>
 			<a href="resources/images/hd-125/sym-hd-125-2.jpg" title="SYM HD 125 EVO">
-			<img src="resources/images/hd-125/sym-hd-125-2.jpg" class="img-polaroid" alt="SYM HD 125 EVO image2" style="width: 100px; height: 100px;">
+			<img src="resources/images/hd-125/sym-hd-125-2.jpg" class="img-polaroid" alt="SYM HD 125 EVO image2" style="width: 120px; height: 100px;">
 			</a>
 			<a href="resources/images/hd-125/sym-hd-125-3.jpg" title="SYM HD 125 EVO">
-			<img src="resources/images/hd-125/sym-hd-125-3.jpg" class="img-polaroid" alt="SYM HD 125 EVO image3" style="width: 100px; height: 100px;">
+			<img src="resources/images/hd-125/sym-hd-125-3.jpg" class="img-polaroid" alt="SYM HD 125 EVO image3" style="width: 120px; height: 100px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-4.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-4.jpg" class="img-polaroid" alt="SYM HD 125 EVO image4" style="width: 120px; height: 100px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-5.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-5.jpg" class="img-polaroid" alt="SYM HD 125 EVO image5" style="width: 100px; height: 100px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-6.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-6.jpg" class="img-polaroid" alt="SYM HD 125 EVO image6" style="width: 100px; height: 100px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-7.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-7.jpg" class="img-polaroid" alt="SYM HD 125 EVO image7" style="width: 100px; height: 140px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-8.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-8.jpg" class="img-polaroid" alt="SYM HD 125 EVO image8" style="width: 100px; height: 140px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-9.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-9.jpg" class="img-polaroid" alt="SYM HD 125 EVO image9" style="width: 100px; height: 100px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-10.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-10.jpg" class="img-polaroid" alt="SYM HD 125 EVO image10" style="width: 100px; height: 100px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-11.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-11.jpg" class="img-polaroid" alt="SYM HD 125 EVO image11" style="width: 100px; height: 100px;">
+			</a>
+			<a href="resources/images/hd-125/sym-hd-125-12.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-12.jpg" class="img-polaroid" alt="SYM HD 125 EVO image12" style="width: 100px; height: 100px;">
+			</a>
+						<a href="resources/images/hd-125/sym-hd-125-13.jpg" title="SYM HD 125 EVO">
+			<img src="resources/images/hd-125/sym-hd-125-13.jpg" class="img-polaroid" alt="SYM HD 125 EVO image13" style="width: 100px; height: 100px;">
 			</a>
 		</ul>
 		</div>
@@ -35,7 +65,7 @@
 				<tbody>
 					<tr>
 						<td width="40%" valign="top" height="18" class="text_bold">ТИП ДВИГАТЕЛ</td>
-						<td width="60%" valign="top" class="text">Едноцилиндров, 4-тактов, 4-клапана, керамична наслойка на цилиндъра</td>
+						<td width="60%" valign="top" class="text">Едноцилиндров, 4-тактов, 4-клапана</td>
 					</tr>
 					<tr class="scheda_tnt">
 						<td width="30%" valign="top" height="18" class="text_bold">РАБОТЕН ОБЕМ</td>
@@ -63,7 +93,7 @@
 					</tr>
 					<tr class="scheda_tnt">
 						<td width="30%" valign="top" height="18" class="text_bold">ПРЕДНА СПИРАЧКА</td>
-						<td width="70%" valign="top" class="text">Дискова &oslash;226 mm</td>
+						<td width="70%" valign="top" class="text">Дискова &oslash;220 mm</td>
 					</tr>
 					<tr class="scheda_tnt">
 						<td width="30%" valign="top" height="18" class="text_bold">ЗАДНА СПИРАЧКА</td>
@@ -79,19 +109,15 @@
 					</tr>
 					<tr class="scheda_tnt">
 						<td width="30%" valign="top" height="18" class="text_bold">РАЗМЕРИ ДхШхВ(mm)</td>
-						<td width="70%" valign="top" class="text">2070х760х1240</td>
-					</tr>
-					<tr class="scheda_tnt">
-						<td valign="top" height="18" class="text_bold">МЕЖДУОСИЕ</td>
-						<td valign="top" class="text">1380 mm</td>
+						<td width="70%" valign="top" class="text">2064х706х1165</td>
 					</tr>
 					<tr class="scheda_tnt">
 						<td valign="top" height="18" class="text_bold">НЕТО ТЕГЛО</td>
-						<td valign="top" class="text">145 kg</td>
+						<td valign="top" class="text">125 kg</td>
 					</tr>
 					<tr class="scheda_tnt">
 						<td valign="top" height="18" class="text_bold">РЕЗЕРВОАР ЗА ГОРИВО</td>
-						<td valign="top" class="text">7.8 l</td>
+						<td valign="top" class="text">8 l</td>
 					</tr>
 				</tbody>
 			</table>
